5ed332d87c7587d02d0f894c9f3336d16cd137cc
Colten Rouska <crouska@leisurelink.com>
2015-09-17 20:18:56 -0600

    ES6-ifying the crap out of this

--- tests/logger-test.js
+++ tests/logger-test.js
@@ -1,54 +1,80 @@
-'use strict';
-
 var chai = require('chai');
 var expect = chai.expect;
+var Loggins = require('../index');
 
 describe('loggins', function(){
-  var logger;
-  before(function(){
-    logger = require('../lib');
-  });
-
-  afterEach(function(){
-    logger.transports = {};
+  it("doesn't throw any exceptions for new object", function(){
+    expect(function(){
+      new Loggins();
+    }).to.not.throw();
   });
 
   describe('defaults', function(){
+    var logger;
     beforeEach(function(){
-      logger.init();
+      logger = new Loggins();
     });
 
-    afterEach(function(){
-      logger.transports = {};
+    it('defaults file as a transport', function(){
+      expect(logger.transports.file).to.be.ok;
     });
 
-    it('should set up defaults for file logger when no settings are supplied', function(){
-      var transports = logger.transports;
-      expect(transports.file).to.have.property('filename').that.equals('all-logs.log');
+    it('defaults console as a transport', function(){
+      expect(logger.transports.console).to.be.ok;
     });
 
-    it('should set up defaults for console logger when no settings are supplied', function(){
-      var transports = logger.transports;
-      expect(transports.console).to.have.property('prettyPrint').that.equals(true);
+    describe('File transport', function(){
+      it('has the defaults set from transport-defaults', function(){
+        var file = logger.transports.file;
+        expect(file).to.have.property('filename').and.equal('all-logs.log');
+        expect(file).to.have.property('level').and.equal('debug');
+        expect(file).to.have.property('handleExceptions').to.equal(true);
+        expect(file).to.have.property('json').and.to.equal(true);
+        expect(file).to.have.property('maxsize').and.to.equal(5242880);
+        expect(file).to.have.property('colorize').and.to.equal(false);
+      });
+    });
+
+    describe('Console transport', function(){
+      it('has the defaults set from transport-defaults', function(){
+        var consl = logger.transports.console;
+        expect(consl).to.have.property('timestamp').and.to.equal(true);
+        expect(consl).to.have.property('prettyPrint').and.to.equal(true);
+        expect(consl).to.have.property('depth').and.to.equal(1);
+        expect(consl).to.have.property('level').and.to.equal('info');
+        expect(consl).to.have.property('handleExceptions').and.to.equal(true);
+        expect(consl).to.have.property('colorize').and.to.equal(true);
+      });
     });
   });
 
-  describe('overriding', function(){
-    afterEach(function(){
-      logger.transports = {};
+  describe('options', function(){
+
+    describe('One without the other', function(){
+      it("doesn't specify file if it isn't there", function(){
+        var logger = new Loggins({ console: {} });
+        expect(logger.transports).to.not.have.property('file');
+      });
+      it("doesn't specify file if it isn't there", function(){
+        var logger = new Loggins({ file: {} });
+        expect(logger.transports).to.not.have.property('console');
+      });
     });
 
-    it('should override the default filename for file', function(){
-      logger.init({ file: { filename: 'lol.log' } });
-      var file = logger.transports.file;
-      expect(file.filename).to.equal('lol.log');
+    describe('file', function(){
+      it('sets the filename to lol.log', function(){
+        var logger = new Loggins({ file: { filename: 'lol.log' } });
+        var file = logger.transports.file;
+        expect(file).to.have.property('filename').and.to.equal('lol.log');
+      });
     });
 
-    it('should only have console if console is specified', function(){
-      logger.init({ console: {} });
-      var transports = logger.transports;
-      expect(transports).to.have.property('console');
-      expect(transports).to.not.have.property('file');
+    describe('console', function(){
+      it('sets the prettyPrint to false', function(){
+        var logger = new Loggins({ console: { prettyPrint: false } });
+        var consl = logger.transports.console;
+        expect(consl).to.have.property('prettyPrint').and.to.equal(false);
+      });
     });
   });
 });
